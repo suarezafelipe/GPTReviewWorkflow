64c256336fae682d5d652d249573cdb8a005f9d7
Felipe <suarez.a.felipe@gmail.com>
2023-04-11 19:44:53 +0400

    Update prompt

--- review.py
+++ review.py
@@ -23,7 +23,7 @@ def get_review():
     explanation = f"Each patch entry has the commit message in the Subject line followed by the code changes (diffs) in a unidiff format.\n"
     patch_info = f"Patch of the Pull Request to review:\n\n{PR_PATCH}\n"
     task_headline = f"As a code reviewer, your task is:\n"
-    task_list = f"- Review the code changes (diffs) and provide feedback.\n- If there are any bugs, highlight them.\n- Do not highlight minor issues and nitpicks.\n- View this as one pull request and don't mention individual patches.\n- Look out for typos in repeating variables.\n- Use markdown formatting.\n- Use bullet points if you have multiple comments.\n"
+    task_list = f"- Review the code changes (diffs) and provide feedback.\n- You can analyze the whole file, or even the whole project for context, but focus your feedback only on the submitted patch and code changes (diffs).\n- If there are any bugs, highlight them.\n- Do not highlight minor issues and nitpicks.\n- View this as one pull request and don't mention individual patches.\n- Look out for typos in repeating variables only in the patch files.\n- Use markdown formatting.\n- Use bullet points if you have multiple comments.\n- Remember it is ok to not have any comment. In that case answer 'I have checked the Pull Request and there is nothing I would change.'\n"
     prompt = intro + explanation + patch_info + task_headline + task_list
 
     print(f"\nPrompt sent to GPT-3: {prompt}\n")
